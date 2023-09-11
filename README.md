b362dae704216775079c75d04353f9cde32825ec
Serhat Acar <sserhatacarr@gmail.com>
2023-09-11 18:35:27 +0300

    Updated Main.java and README.md files

--- README.md
+++ README.md
@@ -1,35 +1,68 @@
-# flightPrice
-Mesafeye ve indirimlere göre uçuş ücretini hesaplar.
+# Flight Price Calculator
 
-Java ile mesafeye ve şartlara göre uçak bileti fiyatı hesaplayan programı yapın. Kullanıcıdan Mesafe (KM), yaşı ve yolculuk tipi (Tek Yön, Gidiş-Dönüş) bilgilerini alın. Mesafe başına ücret 0,10 TL / km olarak alın. İlk olarak uçuşun toplam fiyatını hesaplayın ve sonrasında ki koşullara göre müşteriye aşağıdaki indirimleri uygulayın ;
+Calculate flight ticket prices based on distance and discounts.
 
-Kullanıcıdan alınan değerler geçerli (mesafe ve yaş değerleri pozitif sayı, yolculuk tipi ise 1 veya 2) olmalıdır. Aksi takdirde kullanıcıya "Hatalı Veri Girdiniz !" şeklinde bir uyarı verilmelidir.
-Kişi 12 yaşından küçükse bilet fiyatı üzerinden %50 indirim uygulanır.
-Kişi 12-24 yaşları arasında ise bilet fiyatı üzerinden %10 indirim uygulanır.
-Kişi 65 yaşından büyük ise bilet fiyatı üzerinden %30 indirim uygulanır.
-Kişi "Yolculuk Tipini" gidiş dönüş seçmiş ise bilet fiyatı üzerinden %20 indirim uygulanır.
-İpucu
-Normal Tutar = Mesafe * 0.10 = 1500 * 0.10 = 150 TL
-Yaş İndirimi = Normal Tutar * Yaş İndirim Oranı = 150 * 0.10= 15 TL
-İndirimli Tutar = Normal Tutar – Yaş İndirimi = 150 – 15 = 135 TL
-Gidiş Dönüş Bilet İndirimi = İndirimli Tutar * 0.20 = 135 * 0.20 = 27 TL
-Toplam Tutar = (135-27)* 2 = 216 TL
-Senaryolar
-Senaryo 1
-Mesafeyi km türünden giriniz : 1500
-Yaşınızı giriniz : 20
-Yolculuk tipini giriniz (1 => Tek Yön , 2 => Gidiş Dönüş ): 2
+## Overview
 
-Toplam Tutar = 216 TL
-Senaryo 2
-Mesafeyi km türünden giriniz : -500
-Yaşınızı giriniz : 1
-Yolculuk tipini giriniz (1 => Tek Yön , 2 => Gidiş Dönüş ): 77
+This Java program calculates the cost of a flight ticket based on the following input criteria:
 
-Hatalı Veri Girdiniz !
-Senaryo 3
-Mesafeyi km türünden giriniz : 200
-Yaşınızı giriniz : 35
-Yolculuk tipini giriniz (1 => Tek Yön , 2 => Gidiş Dönüş ): 1
+- Distance (KM)
+- Age
+- Trip type (One Way or Round Trip)
 
-Toplam Tutar = 20.0 TL
+The base price is set at 0.10 $ per kilometer. The program first calculates the total cost of the flight and then applies discounts based on the following conditions:
+
+- All input values must be valid (positive numbers for distance and age, and trip type 1 or 2). Otherwise, an "Invalid Input!" warning is displayed.
+- If the passenger is under 12 years old, a 50% discount is applied to the ticket price.
+- If the passenger is between 12 and 24 years old, a 10% discount is applied to the ticket price.
+- If the passenger is 65 years old or older, a 30% discount is applied to the ticket price.
+- If the passenger selects "Round Trip" as the trip type, a 20% discount is applied to the ticket price.
+
+## Usage
+
+1. Enter the distance in kilometers.
+2. Enter your age.
+3. Select the trip type:
+    - 1 - One Way
+    - 2 - Round Trip
+
+The program will calculate and display the total cost of the flight.
+
+## Example Scenarios
+
+**Scenario 1:**
+
+Enter the distance in kilometers: 1500
+Enter your age: 20
+Select the trip type (1 => One Way, 2 => Round Trip): 2
+
+Total Cost = 216 $
+
+**Scenario 2:**
+
+Enter the distance in kilometers: -500
+Enter your age: 1
+Select the trip type (1 => One Way, 2 => Round Trip): 77
+
+Invalid Input!
+
+**Scenario 3:**
+
+Enter the distance in kilometers: 200
+Enter your age: 35
+Select the trip type (1 => One Way, 2 => Round Trip): 1
+
+Total Cost = 20.0 $
+
+## Hints
+
+- Base Price = Distance * 0.10
+- Age Discount = Base Price * Age Discount Rate
+- Discounted Price = Base Price - Age Discount
+- Round Trip Discount = Discounted Price * 0.20
+- Total Price = (Discounted Price - Round Trip Discount) * 2
+
+## Author
+
+Serhat Acar
+
--- src/Main.java
+++ src/Main.java
@@ -2,61 +2,58 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int km, age, type;
-        double total;
+        int distance, age, tripType;
+        double totalCost;
 
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Mesafeyi km cinsinden giriniz : ");
-        km = input.nextInt();
-        if (km <= 50) {
-            System.out.println("Mesafe 50 kilometreden az olamaz.");
+        System.out.print("Please enter the distance in kilometers: ");
+        distance = input.nextInt();
+
+        if (distance <= 50) {
+            System.out.println("Distance cannot be less than 50 kilometers.");
         } else {
-            System.out.print("Yaşınızı giriniz : ");
+            System.out.print("Please enter your age: ");
             age = input.nextInt();
+
             if (age < 0) {
-                System.out.println("Lütfen geçerli bir yaş giriniz.");
+                System.out.println("Please enter a valid age.");
             } else {
-                System.out.println("Yolculuk tipini seçiniz:\n1-)Tek Yön\n2-)Gidiş-Dönüş");
-                type = input.nextInt();
+                System.out.println("Select the trip type:\n1-) One Way\n2-) Round Trip");
+                tripType = input.nextInt();
 
-                if (type == 1) {
-                    if (age < 12) {
-                        total = (km * 0.1) * 0.5;
-                        System.out.println("Yolculuk ücretiniz = " + total);
-                    } else if (age <= 24) {
-                        total = (km * 0.1) * 0.9;
-                        System.out.println("Yolculuk ücretiniz = " + total);
-                    } else if (65 <= age) {
-                        total = (km * 0.1) * 0.7;
-                        System.out.println("Yolculuk ücretiniz = " + total);
-                    } else {
-                        total = km * 0.1;
-                        System.out.println("Yolculuk ücretiniz = " + total);
-                    }
-                } else if (type == 2) {
-                    if (age < 12) {
-                        total = (km * 0.1) * 0.5 * 0.8 * 2;
-                        System.out.println("Yolculuk ücretiniz = " + total);
-                    } else if (age <= 24) {
-                        total = (km * 0.1) * 0.9 * 0.8 * 2;
-                        System.out.println("Yolculuk ücretiniz = " + total);
-                    } else if (65 <= age) {
-                        total = (km * 0.1) * 0.7 * 0.8 * 2;
-
-                        System.out.println("Yolculuk ücretiniz = " + total);
-                    } else {
-                        total = (km * 0.1) * 0.8 * 2;
-                        System.out.println("Yolculuk ücretiniz = " + total);
-                    }
-
+                if (tripType == 1) {
+                    totalCost = calculateOneWayCost(distance, age);
+                    System.out.println("Your trip cost = $" + totalCost);
+                } else if (tripType == 2) {
+                    totalCost = calculateRoundTripCost(distance, age);
+                    System.out.println("Your trip cost = $" + totalCost);
                 } else {
-                    System.out.println("Geçerli bir yolculuk tipi giriniz.");
+                    System.out.println("Please enter a valid trip type.");
                 }
-
             }
-
-
         }
+    }
+
+    // Calculate one-way trip cost
+    private static double calculateOneWayCost(int distance, int age) {
+        double baseFare = distance * 0.1;
+        double discount = 1.0;
+
+        if (age < 12) {
+            discount = 0.5;
+        } else if (age <= 24) {
+            discount = 0.9;
+        } else if (age >= 65) {
+            discount = 0.7;
+        }
+
+        return baseFare * discount;
+    }
+
+    // Calculate round trip cost
+    private static double calculateRoundTripCost(int distance, int age) {
+        double oneWayCost = calculateOneWayCost(distance, age);
+        return oneWayCost * 2 * 0.8; // 20% round trip discount
     }
 }
